198821b2a3329e852040ebfe40e18a45fd01aaf2
WM <guseo202@gmail.com>
2022-07-28 13:26:34 +0900

    0728

--- app/src/main/java/com/example/icecream/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/icecream/ui/home/HomeFragment.java
@@ -297,4 +297,76 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         return strBuffer.toString();
     }
 
+    public class HomeGridViewAdapter extends BaseAdapter {
+        String TAG = HomeFragment.class.getSimpleName();
+        ArrayList<HomeItemInfo> item = new ArrayList<HomeItemInfo>();
+
+
+        @Override
+        public int getCount() {
+            return item.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return item.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        public void addItem(HomeItemInfo add_item) {
+            item.add(add_item);
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            final Context context = viewGroup.getContext();
+            final HomeItemInfo myitem = item.get(i);
+
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.item_icon, viewGroup, false);
+
+                TextView tv_name = (TextView) view.findViewById(R.id.tv_name);
+                TextView tv_category = (TextView) view.findViewById(R.id.tv_category);
+                ImageView iv_icon = (ImageView) view.findViewById(R.id.iv_icon);
+
+                tv_name.setText(myitem.getName());
+                tv_category.setText(myitem.getCategory());
+                iv_icon.setImageResource(myitem.getResId());
+
+                Log.d(TAG, "getView() - [ " + i + " ] " + myitem.getName());
+            } else {
+                View view_1 = new View(context);
+                view_1 = (View) view;
+            }
+
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Toast.makeText(context, myitem.getBuyYear() + "\n" + myitem.getBuyMonth() + "\n" + myitem.getBuyDay(), Toast.LENGTH_LONG).show();
+
+                    //데이터 전달
+                    HomeItemDialog to_homeItemDialog = new HomeItemDialog();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("name", myitem.getName());
+                    to_homeItemDialog.setArguments(bundle);
+                    //FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+
+
+                    //팝업 생성
+                    HomeItemDialog homeItemDialog = HomeItemDialog.getInstance();
+                    homeItemDialog.show(getChildFragmentManager(), HomeItemDialog.TAG_EVENT_DIALOG);
+                }
+            });
+
+            //홀리몰리
+
+            return view;
+        }
+    }
+
 }
